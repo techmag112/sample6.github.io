b2b8249d3925b60ba1c1c30d452435221a8c00df
techmag112 <49640766+techmag112@users.noreply.github.com>
2022-12-08 14:47:50 +0300

    Add files via upload

--- task4/script.js
+++ task4/script.js
@@ -1,8 +1,12 @@
 
-const addTextForm = document.querySelector('#text');
+const addTextLink = document.querySelector('#text');
 
-addTextForm.addEventListener('click', (event) => {
+addTextLink.addEventListener('click', (event) => {
     /* Отменим стандартную обработку браузера */
     event.preventDefault(); 
-    addTextForm.textContent = prompt('Введите новый текст ссылки', 'Измените текст ссылки');
+    const textValue = prompt('Введите новый текст ссылки', 'Измените текст ссылки');
+    // Если отправляем пустое поле или жмем отмену, предыдущее присвоение текста ссылки сохраняется
+    if (!(textValue === '' || textValue == null)) {
+        addTextLink.textContent = textValue;
+    }  
 });
