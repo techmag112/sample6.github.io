32000b01fb7210bd52ec3ac74b784b2ab58d3f2a
techmag112 <49640766+techmag112@users.noreply.github.com>
2022-12-09 10:28:43 +0300

    Add files via upload

--- task5/script.js
+++ task5/script.js
@@ -1,19 +1,16 @@
 
 const elmLabel = document.querySelector('[data-label]'),
-      elmButton = document.querySelector('[data-btn]'),
+      elmButton = document.querySelector('.btn'),
       duplicateField = document.querySelector('#duplicateField');
 
-    elmLabel.addEventListener('keydown', event => {
-        /* Отменим стандартную обработку браузера */
-        event.preventDefault(); 
+    elmLabel.addEventListener('keydown', event => { 
         // Проверим что нажали Энтер в поле Label
-        if ( event.key === 'Enter') {
-                duplicateField.textContent = '';
+        if ( event.keyCode === 13 ) { 
+                // Этот момент не понимаю! Почему без отмены стандартного обработчика
+                // textContent не присваивается, а в отладчике - присваивается.
+                event.preventDefault(); 
                 duplicateField.textContent = elmLabel.value;
                 elmLabel.value = '';
-        } else {
-        // Если нет, накапливаем строку    
-            elmLabel.value += event.key;
         }   
     });
 
@@ -23,7 +20,7 @@ const elmLabel = document.querySelector('[data-label]'),
         // Проверим что кликнули на кнопке левой кнопкой
         if ( event.which === 1) {
                 duplicateField.textContent = '';
-                duplicateField.textContent = elmLabel.value;
+                console.log(elmLabel.value);
                 elmLabel.value = '';
         }    
     });
